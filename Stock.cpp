8ff27027882198bddfd7e7565441a6678c33a4a1
ItsMe <231827038+Its-Me573@users.noreply.github.com>
2026-02-10 18:38:45 -0800

    Add getsymbol function, include new comments

--- Stock.cpp
+++ Stock.cpp
@@ -10,8 +10,21 @@ Stock::Stock(std::string symbol, double sharePrice = 0.0){
     shares = 0;
 }
 
+/** add new bought shares to total shares, increment the cost with share number * new sharePrice
+ * @param shares is the number of shares bought
+ * @param sharePrice is the price of new shares bought
+*/
 void Stock::Purchase(int shares, double sharePrice){
     this->shares += shares;
     this->cost =+ this->shares*sharePrice;
 }
+
+/** return the symbol of the stock
+* @return the symbol of the stock 
+*/
+std::string Stock::GetSymbol() const{
+    return this->symbol;
+}
+
+
 
